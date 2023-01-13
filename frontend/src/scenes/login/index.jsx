4b6914def092c895b8ed90d5505089e0f4b2615a
Jan Slivka <jan.slivka.workspace@seznam.cz>
2023-01-13 23:21:30 +0100

    Login front-end update

--- frontend/src/scenes/login/index.jsx
+++ frontend/src/scenes/login/index.jsx
@@ -1,59 +1,107 @@
 import React, { useState } from "react";
-import Paper from "@mui/material/Paper";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import { Box, Typography } from "@mui/material";
-import Login from "../../components/Login";
-import Register from "../../components/Register";
+import { gql, useMutation } from "@apollo/client";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
 
-const SignInOut = () => {
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const paperStyle = { width: 340, margin: "20px auto" };
-  function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
   }
+`;
+
+const Login = ({ handleChange }) => {
+  const btnstyle = { margin: "8px 0" };
+  const paperStyle = { padding: 20, width: 300, margin: "0 auto" };
+  const headerStyle = { margin: 0 };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, { data }] = useMutation(LOGIN_MUTATION);
 
   return (
-    <Paper elevation={20} style={paperStyle}>
-      <Tabs
-        value={value}
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={handleChange}
-        aria-label="disabled tabs example"
-      >
-        <Tab label="Sign In" />
-
-        <Tab label="Sign Up" />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <Login handleChange={handleChange} />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Register />
-      </TabPanel>
-    </Paper>
+    <Grid container justify="center">
+      <Paper style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2 style={headerStyle}>Sign In</h2>
+          <Typography variant="caption" gutterBottom>
+            Please fill this form to create an account!
+          </Typography>
+        </Grid>
+        <FormControl
+          style={{
+            marginTop: 5,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <TextField
+            label="Email"
+            placeholder="xxx@gmail.cz"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </FormControl>
+        <FormControl
+          style={{
+            marginTop: 5,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <TextField
+            label="Password"
+            placeholder="Enter your password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+          />
+        </FormControl>
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+          onClick={() => login({ variables: { email, password } })}
+        >
+          Sign in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password ?</Link>
+        </Typography>
+        <Typography>
+          Do you have an account ?
+          <Link href="#" onClick={() => handleChange("event", 1)}>
+            Sign Up
+          </Link>
+        </Typography>
+      </Paper>
+    </Grid>
   );
 };
 
-export default SignInOut;
+export default Login;
